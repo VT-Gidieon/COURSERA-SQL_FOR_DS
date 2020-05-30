d4adab0282f2413b3a2754f843852ed7b5aa866d
VT-Gidieon <37483863+VT-Gidieon@users.noreply.github.com>
2020-05-31 00:08:28 +0530

    WEEK 3 Answers done

--- WEEK3_QUIZ.sql
+++ WEEK3_QUIZ.sql
@@ -1,5 +1,4 @@
---> Q1) Using a subquery, find the names of all the tracks for the
-   album "Californication".
+--> Q1) Using a subquery, find the names of all the tracks for the album "Californication".
 
 SELECT Name,
        AlbumID
@@ -8,9 +7,8 @@ WHERE AlbumId IN (SELECT AlbumId
     FROM Albums
     WHERE Title = "Californication") ;
     
-------------------------------------------------------------------------------------
---> Q2) Find the total number of invoices for each customer along 
-   with the customer's full name, city and email.
+-------------------------------------------------------------------------------------------------------------------------------
+--> Q2) Find the total number of invoices for each customer along with the customer's full name, city and email.
 
 SELECT FirstName,
        LastName,
@@ -21,9 +19,8 @@ FROM Customers C INNER JOIN Invoices I
 ON C.CustomerId = I.CustomerId
 GROUP BY C.CustomerId ;
 
-------------------------------------------------------------------------------------
---> Q3) Retrieve the track name, album, artist, and trackID for 
-   all the albums.
+-------------------------------------------------------------------------------------------------------------------------------
+-- Q3) Retrieve the track name, album, artist, and trackID for all the albums.
 
 SELECT Tracks.Name,
        A.Name AS Artist,
@@ -34,9 +31,8 @@ ON Tracks.AlbumId = Albums.AlbumId)
 INNER JOIN Artists A
 ON A.ArtistId = Albums.ArtistId) ; 
 
-------------------------------------------------------------------------------------
---> Q4) Retrieve a list with the managers last name, and the last 
-   name of the employees who report to him or her.
+--------------------------------------------------------------------------------------------------------------------------------
+-- Q4) Retrieve a list with the managers last name, and the last name of the employees who report to him or her.
 
 SELECT M.LastName AS Manager, 
        E.LastName AS Employee
@@ -44,7 +40,7 @@ FROM Employees E INNER JOIN Employees M
 ON E.ReportsTo = M.EmployeeID ;
 
 ------------------------------------------------------------------------------------
---> Q5) Find the name and ID of the artists who do not have albums.
+-- Q5) Find the name and ID of the artists who do not have albums.
 
 SELECT Name AS Artist,
        Artists.ArtistId,
@@ -54,10 +50,9 @@ LEFT JOIN Albums
 ON Artists.ArtistId = Albums.ArtistId
 WHERE Album IS NULL ;
 
-------------------------------------------------------------------------------------
---> Q6) Use a UNION to create a list of all the employee's & 
-   customer's first names and last names ordered by the last
-   name in descending order.
+-----------------------------------------------------------------------------------------------------------------------------
+-- Q6) Use a UNION to create a list of all the employee's & customer's first names and last names ordered by the last
+-- name in descending order.
 
 SELECT FirstName,
        LastName
@@ -68,9 +63,8 @@ SELECT FirstName,
 FROM Customers
 ORDER BY LastName DESC ;
 
-------------------------------------------------------------------------------------
---> Q7) See if there are any customers who have a different city
-   listed in their billing city versus their customer city.
+-------------------------------------------------------------------------------------------------------------------------------
+-- Q7) See if there are any customers who have a different city listed in their billing city versus their customer city.
 
 SELECT C.FirstName,
        C.LastName,
